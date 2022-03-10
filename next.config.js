c15f09d70eb68eda454e0a6d65a0a0ab10a1e067
Chuck Meyer <chuck.meyer@algolia.com>
2022-03-09 23:18:12 -0500

    feat: Use Cloudinary for image hosting

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     loader: "default",
-    domains: ["pure-beyond-35461.herokuapp.com"],
+    domains: [
+      "res.cloudinary.com"
+    ],
   }
 };
 
